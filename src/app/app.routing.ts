393a34356f11e25179b46fcf1878b2c7d7cd3bea
hemansin <hemansin>
2017-07-27 23:27:31 +0530

    starter added

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -22,25 +22,9 @@ export const routes: Routes = [
         path: 'dashboard',
         loadChildren: './dashboard/dashboard.module#DashboardModule'
       },
-      {
-        path: 'components',
-        loadChildren: './components/components.module#ComponentsModule'
-      },
-      {
-        path: 'icons',
-        loadChildren: './icons/icons.module#IconsModule'
-      },
-      {
-        path: 'widgets',
-        loadChildren: './widgets/widgets.module#WidgetsModule'
-      },
-      {
-        path: 'charts',
-        loadChildren: './chartjs/chartjs.module#ChartJSModule'
-      }
     ]
   },
-  {
+    {
     path: 'pages',
     component: SimpleLayoutComponent,
     data: {
